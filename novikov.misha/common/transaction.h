a291383b2f504c1c3544de121d2d93c8bd55b5b4
Mishaster82 <misha.novikov2007@gmail.com>
2026-09-24 19:13:09 +0300

    transaction.h done

--- novikov.misha/common/transaction.h
+++ novikov.misha/common/transaction.h
@@ -1,3 +1,28 @@
-#include <iostream>
+#ifndef COMMON_TRANSACTION_H_
+#define COMMON_TRANSACTION_H_
 
-int main(){}
+#include <cstddef>
+#include <istream>
+#include <ostream>
+#include <string>
+
+#include "dynamic_array.h"
+
+namespace novikov {
+
+struct Transaction
+{
+  int from;
+  int to;
+  int value;
+};
+
+bool parseTransactionLine(const std::string &line, Transaction &result);
+
+void readTransactions(std::istream &input, DynamicArray<Transaction> &transactions, std::size_t &ignoredCount);
+
+void writeTransaction(std::ostream &output, const Transaction &transaction);
+
+}
+
+#endif
